dfc312a8071d8baa16556788147cd0b6413052bf
Bryan Lee <78395163+bryan901@users.noreply.github.com>
2022-10-03 01:14:20 -0700

    minor changes for skills section

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -4,7 +4,7 @@ import '../styles/Skill.css'
 function Skill() {
   return (
     <div className = "skill-main-container">
-        <img className ="skill-logo" src="assets/microsoft-logo.png" alt="" />
+        <img className ="skill-logo" src="assets/java-logo.png" alt="" />
         <h1 className = "skill-name-specific">skill</h1>
         <body className = "skill-description-specific">For this skill, 
         i sucked fat dick, and got chlamyudia dn a bunch of other crazy mental illnesses including down sybndrome and retardation</body>
