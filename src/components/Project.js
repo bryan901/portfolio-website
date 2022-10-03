05d42fcd96ebb664daf5a811780de68f0441ca38
Bryan Lee <78395163+bryan901@users.noreply.github.com>
2022-10-03 00:12:15 -0700

    finished basic design for projects/ other activities page

---
+++ src/components/Project.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Project.css'
+
+function Project() {
+  return (
+    <div className = "project-main-container">
+        <img className ="project-logo" src="assets/microsoft-logo.png" alt="" />
+        <h1 className = "project-name-specific">Project1</h1>
+        <body className = "project-description-specific">For this project, i sucked fat dick</body>
+    </div>
+  )
+}
+
+export default Project
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import '../styles/Projects.css'
+import Project from '../components/Project.js'
 
 export default function Projects() {
   return (
-    <div className = "projects " id = "projects">Projects</div>
+    <div className = "projects " id = "projects">
+      <div className = "projects-header">
+        <h1 className = "projects-title-text">personal projects and other activities</h1>
+        <body className = 'projects-subtitle-text'>below are some interest projects I have done in my own time, outiside class, as well as some of the activities I am currently involved in!</body>
+      </div>
+      <div className = "project-container">
+        <div className = "column">
+          <Project/>
+          <Project/>
+        </div>
+        <div className = 'column'>
+          <Project/>
+          <Project/>
+        </div>
+        <div className = 'column'>
+          <Project/>
+          <Project/>
+        </div>
+      </div>
+    </div>
   )
 }
